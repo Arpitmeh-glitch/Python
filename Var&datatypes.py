6afc67859bba717ebef10764dbf8ea13c65598df
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-01-06 14:00:00 +0530

    Variables Declaration And Datatypes With Limits

---
+++ Var&datatypes.py
@@ -0,0 +1,29 @@
+a=0 #this is how you define a variable in python 
+#in python you dont need to specify the datatype of a variable prior to creating it
+#It automatically assigns it a datatype according to the value entered
+#This is called as being dynamically typed
+#Datatypes:
+#int>>
+a=0
+#float (Max: Approximately 1.8 x 10^308. Min:2.2 x 10^-308)
+b=1.2
+#complex(same range as float)
+c=1+2j
+#char(it uses unicode and the max limit is U+10FFFF(idk what this means))
+d="d"
+#str
+e="My name is Nucleophile"
+#Lists
+ls=[1,2,3,4]
+#Tuples(Immutable)
+tup=(1,2,3,4)
+#boolean
+f=True
+#Dictionary
+Dict={
+    "Name":"Arpit Mehrotra",
+    "BATCH":1,
+}
+#Set
+Set={1,2,3,4,5}
+
