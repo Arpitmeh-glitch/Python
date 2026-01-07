d5c8a686cb8103971c610bf2833db582ce18dd45
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-01-07 14:00:00 +0530

    STRING METHODS

---
+++ xyz.py
@@ -0,0 +1,19 @@
+#string methods in python
+st="My name is Arpit Mehrotra"
+print(st.capitalize())
+#>>My name is arpit mehrotra
+print(st.count("a"))
+#>>2
+print(st.upper())
+#>>MY NAME IS ARPIT MEHROTRA
+print(st.lower())
+#>>my name is arpit mehrotra
+print(st.find("n"))
+#>>3
+print(st.index("A"))
+#>>11
+print(st.split())
+#>>['My', 'name', 'is', 'Arpit', 'Mehrotra']
+print(st.endswith("a"))
+#>>True
+#THESE ARE THE COMMON ONES 
