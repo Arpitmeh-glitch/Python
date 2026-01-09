c2b3256bd86116910ebad586150fe4d51ad74820
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-01-09 14:00:00 +0530

    Course Plan Experiment1 Q1-4

---
+++ Experiment1/1.py
@@ -0,0 +1,4 @@
+#1. Steps to installing python:
+# Step1: sudo dnf search python3.14.2
+# Step2 :sudo dnf install python3.14.2
+# Step3 :python3 –version
---
+++ Experiment1/4.py
@@ -0,0 +1,6 @@
+a=12
+name="Arpit"
+gpa=9.9
+print(type(a))
+print(type(gpa))
+print(type(name))
