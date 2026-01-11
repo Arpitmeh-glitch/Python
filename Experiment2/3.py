0bdd77b2fd9d1564c7d8ba82b47351a01f7e49b5
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-01-11 20:01:00 +0530

    Experiment2

---
+++ Experiment2/3.py
@@ -0,0 +1,8 @@
+a=int(input("enter 1st number: "))
+b=int(input("enter 2nd number: "))
+if(a>b):
+    print(a,"is greater")
+elif(b>a):
+    print(b,"is greater")
+else:
+    print("numbers are equal")
