1526cca7d1bb8cb6d496da9a98c870d9ffeabc2d
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-01-12 20:01:00 +0530

    Experiment2

---
+++ Experiment2/4.py
@@ -0,0 +1,9 @@
+a=int(input("enter 1st number: "))
+b=int(input("enter 2nd number: "))
+c=int(input("enter 3rd number: "))
+if((a>b)&(a>c)):
+    print(a)
+elif((b>c)&(b>a)):
+    print(b)
+else:
+    print(c)
