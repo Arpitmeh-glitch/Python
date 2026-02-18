afda7a546e86cac79aa08321c35ffe80109fa6e0
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-02-18 13:41:16 +0530

    q7

---
+++ Experiment4/7.py
@@ -0,0 +1,17 @@
+n = int(input("Enter number of fruits in each set: "))
+s1 = set()
+print("Enter fruits for set s1:")
+for i in range(n):
+    fruit = input()
+    s1.add(fruit)
+s2 = set()
+print("Enter fruits for set s2:")
+for i in range(n):
+    fruit = input()
+    s2.add(fruit)
+common_fruits = s1.intersection(s2)
+only_s1 = s1.difference(s2)
+all_fruits = s1.union(s2)
+print("a) Fruits in both sets:", common_fruits)
+print("b) Fruits only in s1:", only_s1)
+print("c) Count of all fruits:", len(all_fruits))
