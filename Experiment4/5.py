217768a23771be8fef8b73330ffa4537e1325886
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-02-18 13:35:24 +0530

    q7

--- Experiment4/5.py
+++ Experiment4/5.py
@@ -1,2 +1,14 @@
-stri=input("Enter a string to be checked: ")
-stri=stri.lower()
+s = input("Enter a string: ")
+
+s = s.lower()     
+count = {}
+
+for ch in s:
+    if ch.isalpha():   
+        if ch in count:
+            count[ch] += 1
+        else:
+            count[ch] = 1
+
+for key in sorted(count):
+    print(key, ":", count[key])
