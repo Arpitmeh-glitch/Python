43ebaceffd45d7884fcca4aef91df775e9c705b5
Arpitmeh-glitch <mehrotraa245@gmail.com>
2026-02-19 11:09:26 +0530

    exp5

---
+++ Experiment5/1.py
@@ -0,0 +1,6 @@
+n=int(input("Enter the number of elements you want to enter"))
+ls=[]
+for i in range(0,n):
+    ls.append(int(input("Enter the element of the list"))) 
+for i in range (0,n):
+    print(ls)
---
+++ Experiment5/3.py
@@ -0,0 +1,6 @@
+N = int(input("Enter number of students: "))
+scores = list(map(int, input("Enter scores: ").split()))
+unique_scores = list(set(scores))
+unique_scores.sort()
+runner_up = unique_scores[-2]
+print("Runner-up score is:", runner_up)
